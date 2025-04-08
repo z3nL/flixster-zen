84b59ab79ccc13bc4eefd58d8cda1860bf732401
kyra-ptn <kyra@codepath.org>
2025-04-08 16:39:04 -0500

    Add static data

---
+++ src/data/data.js
@@ -0,0 +1,401 @@
+const data = {
+    "page": 1,
+    "results": [
+    {
+        "adult": false,
+        "backdrop_path": "/2Nti3gYAX513wvhp8IiLL6ZDyOm.jpg",
+        "genre_ids": [
+        10751,
+        35,
+        12,
+        14
+        ],
+        "id": 950387,
+        "original_language": "en",
+        "original_title": "A Minecraft Movie",
+        "overview": "Four misfits find themselves struggling with ordinary problems when they are suddenly pulled through a mysterious portal into the Overworld: a bizarre, cubic wonderland that thrives on imagination. To get back home, they'll have to master this world while embarking on a magical quest with an unexpected, expert crafter, Steve.",
+        "popularity": 1022.7906,
+        "poster_path": "/yFHHfHcUgGAxziP1C3lLt0q2T4s.jpg",
+        "release_date": "2025-04-04",
+        "title": "A Minecraft Movie",
+        "video": false,
+        "vote_average": 6.062,
+        "vote_count": 298
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/lqHt4icP1GTaNBeVTxTrwTZdoAW.jpg",
+        "genre_ids": [
+        28,
+        53,
+        9648,
+        80
+        ],
+        "id": 1195430,
+        "original_language": "hi",
+        "original_title": "देवा",
+        "overview": "Dev Ambre, a ruthless cop, loses his memory in an accident just after he has finished solving a murder case and now has to reinvestigate it while keeping his memory loss a secret from everyone except DCP Farhan Khan.",
+        "popularity": 485.463,
+        "poster_path": "/4wKpglgZYMYpISMdThgdqS1TSQc.jpg",
+        "release_date": "2025-01-31",
+        "title": "Deva",
+        "video": false,
+        "vote_average": 3.2,
+        "vote_count": 13
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/is9bmV6uYXu7LjZGJczxrjJDlv8.jpg",
+        "genre_ids": [
+        28,
+        12
+        ],
+        "id": 1229730,
+        "original_language": "fr",
+        "original_title": "Carjackers",
+        "overview": "By day, they're invisible—valets, hostesses, and bartenders at a luxury hotel. By night, they're the Carjackers, a crew of skilled drivers who track and rob wealthy clients on the road. As they plan their ultimate heist, the hotel director hires a ruthless hitman, to stop them at all costs. With danger closing in, can Nora, Zoe, Steve, and Prestance pull off their biggest score yet?",
+        "popularity": 380.9912,
+        "poster_path": "/wbkPMTz2vVai7Ujyp0ag7AM9SrO.jpg",
+        "release_date": "2025-03-27",
+        "title": "Carjackers",
+        "video": false,
+        "vote_average": 6.093,
+        "vote_count": 81
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/gsQJOfeW45KLiQeEIsom94QPQwb.jpg",
+        "genre_ids": [
+        28,
+        53
+        ],
+        "id": 1125899,
+        "original_language": "en",
+        "original_title": "Cleaner",
+        "overview": "When a group of radical activists take over an energy company's annual gala, seizing 300 hostages, an ex-soldier turned window cleaner suspended 50 storeys up on the outside of the building must save those trapped inside, including her younger brother.",
+        "popularity": 343.3057,
+        "poster_path": "/mwzDApMZAGeYCEVjhegKvCzDX0W.jpg",
+        "release_date": "2025-02-21",
+        "title": "Cleaner",
+        "video": false,
+        "vote_average": 6.517,
+        "vote_count": 174
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/8eifdha9GQeZAkexgtD45546XKx.jpg",
+        "genre_ids": [
+        28,
+        53,
+        878
+        ],
+        "id": 822119,
+        "original_language": "en",
+        "original_title": "Captain America: Brave New World",
+        "overview": "After meeting with newly elected U.S. President Thaddeus Ross, Sam finds himself in the middle of an international incident. He must discover the reason behind a nefarious global plot before the true mastermind has the entire world seeing red.",
+        "popularity": 339.0809,
+        "poster_path": "/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg",
+        "release_date": "2025-02-14",
+        "title": "Captain America: Brave New World",
+        "video": false,
+        "vote_average": 6.087,
+        "vote_count": 1233
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/AlpDRp3Qaf2JvlQDzXS8kiYxTf8.jpg",
+        "genre_ids": [
+        28,
+        53
+        ],
+        "id": 1212855,
+        "original_language": "en",
+        "original_title": "Fight or Flight",
+        "overview": "A mercenary takes on the job of tracking down a target on a plane but must protect her when they're surrounded by people trying to kill both of them.",
+        "popularity": 298.5071,
+        "poster_path": "/x4nWnfgJvL045rcUCSJzfgIIY9i.jpg",
+        "release_date": "2025-05-09",
+        "title": "Fight or Flight",
+        "video": false,
+        "vote_average": 4.152,
+        "vote_count": 23
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/xBrx6O1RIhjtWmVthZIANuQz7Z2.jpg",
+        "genre_ids": [
+        28,
+        80,
+        53
+        ],
+        "id": 1197306,
+        "original_language": "en",
+        "original_title": "A Working Man",
+        "overview": "Levon Cade left behind a decorated military career in the black ops to live a simple life working construction. But when his boss's daughter, who is like family to him, is taken by human traffickers, his search to bring her home uncovers a world of corruption far greater than he ever could have imagined.",
+        "popularity": 293.7838,
+        "poster_path": "/6FRFIogh3zFnVWn7Z6zcYnIbRcX.jpg",
+        "release_date": "2025-03-28",
+        "title": "A Working Man",
+        "video": false,
+        "vote_average": 6.5,
+        "vote_count": 151
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/qiVrapzgrDO7c6yQTSvn0dhwsn8.jpg",
+        "genre_ids": [
+        10749,
+        18
+        ],
+        "id": 1252309,
+        "original_language": "es",
+        "original_title": "Pídeme lo que quieras",
+        "overview": "After his father's death, Eric Zimmerman travels to Spain to oversee his company's branches. In Madrid, he falls for Judith and engage in an intense, erotic relationship full of exploration. However, Eric fears his secret may end their affair.",
+        "popularity": 286.593,
+        "poster_path": "/76qnVxU2rPdVvipBN3DPQH6fVYB.jpg",
+        "release_date": "2024-11-29",
+        "title": "Ask Me What You Want",
+        "video": false,
+        "vote_average": 5.681,
+        "vote_count": 119
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/9OO6I25MHhCDd0XwNk4oZl0fpwB.jpg",
+        "genre_ids": [
+        28,
+        18
+        ],
+        "id": 1261050,
+        "original_language": "da",
+        "original_title": "De lydløse",
+        "overview": "In 2008, a group of men from Denmark and across Europe pull off the biggest heist of all time on Danish soil. Kasper, a boxer with few chances left in life, is offered the opportunity to plan the robbery by its foreign initiators.",
+        "popularity": 267.1198,
+        "poster_path": "/7NLY1jNwtZX1yVzwVoBeAhaBE8i.jpg",
+        "release_date": "2025-02-21",
+        "title": "The Quiet Ones",
+        "video": false,
+        "vote_average": 4.828,
+        "vote_count": 29
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/ibF5XVxTzf1ayzZrmiJqgeQ39Qk.jpg",
+        "genre_ids": [
+        28,
+        10752
+        ],
+        "id": 1373723,
+        "original_language": "en",
+        "original_title": "The Codes of War",
+        "overview": "War stories about family, ethics and honor include the true story of two U.S. Marines who in a span of six seconds, must stand their ground to stop a suicide truck bomb, a Navy Corpsman who attempts to hold on to his humanity, and a WW2 soldier who gets separated from his squad and is forced to re-evaluate his code.",
+        "popularity": 254.8803,
+        "poster_path": "/oXeiQAfRK90pxxhP5iKPXQqAIN1.jpg",
+        "release_date": "2025-03-20",
+        "title": "The Codes of War",
+        "video": false,
+        "vote_average": 6.088,
+        "vote_count": 40
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/qUc0Hol3eP74dbW4YyqT6oRLYgT.jpg",
+        "genre_ids": [
+        878,
+        35,
+        12
+        ],
+        "id": 696506,
+        "original_language": "en",
+        "original_title": "Mickey 17",
+        "overview": "Unlikely hero Mickey Barnes finds himself in the extraordinary circumstance of working for an employer who demands the ultimate commitment to the job… to die, for a living.",
+        "popularity": 246.8131,
+        "poster_path": "/edKpE9B5qN3e559OuMCLZdW1iBZ.jpg",
+        "release_date": "2025-03-07",
+        "title": "Mickey 17",
+        "video": false,
+        "vote_average": 6.954,
+        "vote_count": 1259
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/m2mzlsJjE3UAqeUB5fLUkpWg4Iq.jpg",
+        "genre_ids": [
+        53,
+        878
+        ],
+        "id": 1165067,
+        "original_language": "en",
+        "original_title": "Cosmic Chaos",
+        "overview": "Battles in virtual reality, survival in a post-apocalyptic wasteland, a Soviet spaceship giving a distress signal - Fantastic stories created with advanced special effects and passion.",
+        "popularity": 245.4771,
+        "poster_path": "/mClzWv7gBqgXfjZXp49Enyoex1v.jpg",
+        "release_date": "2023-08-03",
+        "title": "Cosmic Chaos",
+        "video": false,
+        "vote_average": 5.711,
+        "vote_count": 38
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/nhx0ttmAS2w8e6hyIcKoNvgHoix.jpg",
+        "genre_ids": [
+        27,
+        53,
+        14
+        ],
+        "id": 1020414,
+        "original_language": "en",
+        "original_title": "Peter Pan's Neverland Nightmare",
+        "overview": "Wendy Darling strikes out in an attempt to rescue her brother Michael from the clutches of the evil Peter Pan who intends to send him to Neverland. Along the way she meets a twisted Tinkerbell, who is hooked on what she thinks is fairy dust.",
+        "popularity": 214.9261,
+        "poster_path": "/mOR1Ks0EcXocwMV4EPv4letz0F5.jpg",
+        "release_date": "2025-01-13",
+        "title": "Peter Pan's Neverland Nightmare",
+        "video": false,
+        "vote_average": 5.14,
+        "vote_count": 43
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/fzv87rT0jlAkh5Uf9PpIlUj6Nj8.jpg",
+        "genre_ids": [
+        28,
+        53,
+        80
+        ],
+        "id": 1195585,
+        "original_language": "en",
+        "original_title": "The Hard Hit",
+        "overview": "An Interpol agent hunting the head of a global crime syndicate tracks his target to Las Vegas, but when the criminal organization kills his wife and daughter, he goes above the law to get his revenge.",
+        "popularity": 204.9709,
+        "poster_path": "/whkFbOZTamHeugEG95jvQehSzAH.jpg",
+        "release_date": "2023-10-20",
+        "title": "The Hard Hit",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/zo8CIjJ2nfNOevqNajwMRO6Hwka.jpg",
+        "genre_ids": [
+        16,
+        12,
+        10751,
+        35
+        ],
+        "id": 1241982,
+        "original_language": "en",
+        "original_title": "Moana 2",
+        "overview": "After receiving an unexpected call from her wayfinding ancestors, Moana journeys alongside Maui and a new crew to the far seas of Oceania and into dangerous, long-lost waters for an adventure unlike anything she's ever faced.",
+        "popularity": 197.6746,
+        "poster_path": "/aLVkiINlIeCkcZIzb7XHzPYgO6L.jpg",
+        "release_date": "2024-11-27",
+        "title": "Moana 2",
+        "video": false,
+        "vote_average": 7.082,
+        "vote_count": 2166
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/1w8kutrRucTd3wlYyu5QlUDMiG1.jpg",
+        "genre_ids": [
+        12,
+        10751,
+        16
+        ],
+        "id": 762509,
+        "original_language": "en",
+        "original_title": "Mufasa: The Lion King",
+        "overview": "Mufasa, a cub lost and alone, meets a sympathetic lion named Taka, the heir to a royal bloodline. The chance meeting sets in motion an expansive journey of a group of misfits searching for their destiny.",
+        "popularity": 188.9034,
+        "poster_path": "/lurEK87kukWNaHd0zYnsi3yzJrs.jpg",
+        "release_date": "2024-12-20",
+        "title": "Mufasa: The Lion King",
+        "video": false,
+        "vote_average": 7.415,
+        "vote_count": 1892
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/zvWnh0ABsJlco2tikliiATEKhLN.jpg",
+        "genre_ids": [
+        27,
+        53
+        ],
+        "id": 1249385,
+        "original_language": "en",
+        "original_title": "Ghost Game",
+        "overview": "As part of an internet challenge to live undetected in a stranger’s home, a daring couple target an infamous haunted house and endure a series of chilling incidents as they witness a family descending into madness.",
+        "popularity": 170.0659,
+        "poster_path": "/kxxWBEn7qTN2xumSferCHbDudcT.jpg",
+        "release_date": "2024-10-18",
+        "title": "Ghost Game",
+        "video": false,
+        "vote_average": 3.333,
+        "vote_count": 18
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/wi2CEFSyoxoBWdFeVmTeqnlX3qI.jpg",
+        "genre_ids": [
+        18,
+        9648,
+        53
+        ],
+        "id": 1233575,
+        "original_language": "en",
+        "original_title": "Black Bag",
+        "overview": "When intelligence agent Kathryn Woodhouse is suspected of betraying the nation, her husband – also a legendary agent – faces the ultimate test of whether to be loyal to his marriage, or his country.",
+        "popularity": 168.4294,
+        "poster_path": "/hHPovtU4b96LHcoeEwRkGHI5btw.jpg",
+        "release_date": "2025-03-14",
+        "title": "Black Bag",
+        "video": false,
+        "vote_average": 6.366,
+        "vote_count": 280
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/o5vasl0xbZWWKQnAlaBTSgntHH2.jpg",
+        "genre_ids": [
+        27
+        ],
+        "id": 1208808,
+        "original_language": "es",
+        "original_title": "Turno nocturno",
+        "overview": "A young nurse with a domestic abuse past starts working the night shift at a small hospital, where, during the night, the ghost of a nurse materializes.",
+        "popularity": 167.6628,
+        "poster_path": "/iSSx9Bys64vlOkvkyKXtp19P7Re.jpg",
+        "release_date": "2024-10-31",
+        "title": "Turno nocturno",
+        "video": false,
+        "vote_average": 6.387,
+        "vote_count": 53
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/e807jDKiFcntZS32ze88X6I96OD.jpg",
+        "genre_ids": [
+        16,
+        28
+        ],
+        "id": 1297763,
+        "original_language": "ja",
+        "original_title": "ニンジャバットマン対ヤクザリーグ",
+        "overview": "The Batman family has returned to the present to discover that Japan has disappeared, and a giant island - Hinomoto - is now in the sky over Gotham City.  At the top sit the Yakuza, a group of superpowered individuals who reign without honor or humanity and look suspiciously like the Justice League. Now, it’s up to Batman and his allies to save Gotham!",
+        "popularity": 158.7616,
+        "poster_path": "/sVVT6GYFErVv0Lcc9NvqCu0iOxO.jpg",
+        "release_date": "2025-03-18",
+        "title": "Batman Ninja vs. Yakuza League",
+        "video": false,
+        "vote_average": 6.654,
+        "vote_count": 117
+    }
+    ],
+    "total_pages": 49622,
+    "total_results": 992423
+}
+
+export default data
