a673cdc274876d51933d7ed7dba98038b8a97e0c
Kyra Patton <144150344+kyra-ptn@users.noreply.github.com>
2025-05-12 12:41:09 -0500

    Wrap in return statement

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  <div className="App">
+  return (
+    <div className="App">
     
   </div>
+    )
 }
 
 export default App
